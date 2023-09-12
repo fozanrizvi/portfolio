2c7afeaabbb41a529e465b85dfbe4b815cfad71c
Fozan Rizvi <fozanrizvi@gmail.com>
2023-09-12 18:10:14 -0500

    some templates in the sidebar

---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <>
+    {/* Homepage */}
+    {/* About me */}
+    {/* Shelf/projects/links/writings */}
+
+    {/* Github */}
+    {/* LinkedIn */}
+    {/* instagram */}
+
+    {/* Contact me */}
+    {/* Hobbies/skydiving/reading/trails */}
+    </>
+  )
+}
+
+export default SideBar;
