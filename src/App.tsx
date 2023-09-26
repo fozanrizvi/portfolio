2e29ff714202a9b7c3fe826997157f934cf40156
Fozan Rizvi <fozanrizvi@gmail.com>
2023-09-26 05:49:52 -0500

    design fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import HomePage from './components/HomePage/HomePage';
 
-import { CenterDiv } from './App.styled';
+import { FlexDiv } from './App.styled';
+import AboutMe from './components/aboutMe/AboutMe';
+import Shelf from './components/shelf/Shelf';
+import Github from './components/github/Github';
+import ContactMe from './components/contactMe/ContactMe';
+import Hobbies from './components/hobbies/Hobbies';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <CenterDiv>
+    <FlexDiv>
+      <Navbar />
+      <div style={{ width: '100%'}}>                
       <HomePage />
-    </CenterDiv>
+      <AboutMe />
+      <Shelf />
+      <Github />
+      <ContactMe />
+      <Hobbies />
+      </div>
+      
+    </FlexDiv>
   );
 }
 
