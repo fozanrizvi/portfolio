b698f59ab6c668662209c633d40a833aa0cf942a
Fozan Rizvi <fozanrizvi@gmail.com>
2023-09-26 05:53:12 -0500

    add hooks

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,48 +1,38 @@
 import { Menu, MenuItem } from 'react-pro-sidebar';
 
 //Material icons
+import MenuIcon from '@mui/icons-material/Menu';
 import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import RingVolumeIcon from '@mui/icons-material/RingVolume';
 import SportsBasketballIcon from '@mui/icons-material/SportsBasketball';
 import { Sidebar } from './Navbar.styled';
+import { IconButton } from '@mui/material';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const handleCollapse = () => {
+    setIsCollapsed(!isCollapsed)
+  }
+
   return (
     <>
-    {/* <CustomSidebar>
+   
+    <Sidebar isCollapsed={isCollapsed}>
+      <IconButton onClick={handleCollapse} >
+        <MenuIcon/>
+      </IconButton>
       <Menu>
-        <MenuItem icon={<ContactsOutlinedIcon />} href='/'>About me</MenuItem>
-        <MenuItem icon={<LibraryBooksIcon />} href='/'>Shelf</MenuItem>
-        <MenuItem icon={<GitHubIcon />} href='/'>Github</MenuItem>
-        <MenuItem icon={<RingVolumeIcon />} href='/'>Contact Me</MenuItem>
-        <MenuItem icon={<SportsBasketballIcon />} href='/'>Hobbies</MenuItem>
-      </Menu>
-    </CustomSidebar> */}
-    <Sidebar >
-    <Menu>
-        <MenuItem icon={<ContactsOutlinedIcon />} href='/'>About me</MenuItem>
-        <MenuItem icon={<LibraryBooksIcon />} href='/'>Shelf</MenuItem>
-        <MenuItem icon={<GitHubIcon />} href='/'>Github</MenuItem>
-        <MenuItem icon={<RingVolumeIcon />} href='/'>Contact Me</MenuItem>
-        <MenuItem icon={<SportsBasketballIcon />} href='/'>Hobbies</MenuItem>
+        <MenuItem icon={<ContactsOutlinedIcon />} href='#aboutMe'>About me</MenuItem>
+        <MenuItem icon={<LibraryBooksIcon />} href='#shelf'>Shelf</MenuItem>
+        <MenuItem icon={<GitHubIcon />} href='#github'>Github</MenuItem>
+        <MenuItem icon={<RingVolumeIcon />} href='contactMe'>Contact Me</MenuItem>
+        <MenuItem icon={<SportsBasketballIcon />} href='hobbies'>Hobbies</MenuItem>
       </Menu>
     </Sidebar>
-      {/* {navigationLinks.map((item: any)=>{
-              <Link href={item.href}>{item.name}</Link>
-            })} */}
-
-      { /* Homepage */ }
-  { /* About me */ }
-  { /* Shelf/projects/links/writings */ }
-
-  { /* Github */ }
-  { /* LinkedIn */ }
-  { /* instagram */ }
-
-  { /* Contact me */ }
-  { /* Hobbies/skydiving/reading/trails */ }
   </>
 )};
 
