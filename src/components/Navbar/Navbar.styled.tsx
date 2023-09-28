ca955548e817fd52e53d5b842627fde0665acb9e
Fozan Rizvi <fozanrizvi@gmail.com>
2023-09-27 21:08:33 -0500

    github api integration

--- src/components/Navbar/Navbar.styled.tsx
+++ src/components/Navbar/Navbar.styled.tsx
@@ -1,12 +1,17 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { IconButton } from "@mui/material";
 
-interface Props{
-    isCollapsed: boolean;
+interface Props {
+  isCollapsed: boolean;
 }
 
 export const Sidebar = styled.div<Props>`
-    height: 100vh;
-    background-color: lightgrey;
-    position: fixed;
-    width: ${props => props.isCollapsed ? '80px' : 'auto'};
+  height: 100vh;
+  background-color: transparent;
+  position: fixed;
+  width: ${(props) => (props.isCollapsed ? "80px" : "auto")};
+`;
+
+export const StyledIconButton = styled(IconButton)`
+  left: 15px;
 `;
