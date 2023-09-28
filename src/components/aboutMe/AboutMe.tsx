6a3df378598526180c543204c19bc09bcf122eae
Fozan Rizvi <fozanrizvi@gmail.com>
2023-09-27 21:37:58 -0500

    adding some details to comps

--- src/components/aboutMe/AboutMe.tsx
+++ src/components/aboutMe/AboutMe.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import { Section } from '../shared/Section.styled';
+import React from "react";
+import { Section } from "../shared/Section.styled";
 
 const AboutMe = () => {
   return (
     <Section id="aboutMe">
       <h1>About me</h1>
+      <div>{`I possess strong web development skills, designed and developed 
+      dynamic web applications using React, utilizing Typescript. 
+      Built extremely competent cross-platform single page applications
+      using component-based architecture made up of several re-usable
+      components, with each of them having its particular logic, written
+      in JavaScript. Also created responsive web page designs and layouts
+      using media queries, cascading style sheets grid layout and 
+      styled-components. Strong working knowledge of ES6 using classes, 
+      arrow functions, de-structuring assignments, promises, async/await, 
+      object-oriented and functional programming.`}</div>
     </Section>
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;
