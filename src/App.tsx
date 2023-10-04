721b308353a78fdaade29e0c2293ed5c870739f9
Fozan Rizvi <fozanrizvi@gmail.com>
2023-10-04 16:12:44 -0500

    adding skills section in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import HomePage from './components/HomePage/HomePage';
+import HomePage from "./components/HomePage/HomePage";
+import { FlexDiv } from "./App.styled";
 
-import { FlexDiv } from './App.styled';
-import AboutMe from './components/aboutMe/AboutMe';
-import Shelf from './components/shelf/Shelf';
-import Github from './components/github/Github';
-import ContactMe from './components/contactMe/ContactMe';
-import Hobbies from './components/hobbies/Hobbies';
-import Navbar from './components/Navbar/Navbar';
+//components
+import Navbar from "./components/Navbar/Navbar";
+import AboutMe from "./components/aboutMe/AboutMe";
+import Shelf from "./components/shelf/Shelf";
+import Github from "./components/github/Github";
+import Skills from "./components/skills/Skills";
+import ContactMe from "./components/contactMe/ContactMe";
+import Hobbies from "./components/hobbies/Hobbies";
 
 function App() {
   return (
     <FlexDiv>
       <Navbar />
-      <div style={{ width: '100%'}}>                
-      <HomePage />
-      <AboutMe />
-      <Shelf />
-      <Github />
-      <ContactMe />
-      <Hobbies />
+      <div style={{ width: "100%" }}>
+        <HomePage />
+        <AboutMe />
+        <Shelf />
+        <Github />
+        <Skills />
+        <ContactMe />
+        <Hobbies />
       </div>
-      
     </FlexDiv>
   );
 }
