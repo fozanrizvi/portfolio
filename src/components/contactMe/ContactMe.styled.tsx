fc5744ba14d511675b1bda71b828b86fe085a468
Fozan Rizvi <fozanrizvi@gmail.com>
2023-10-07 11:39:05 -0500

    integrating social media links

--- src/components/contactMe/ContactMe.styled.tsx
+++ src/components/contactMe/ContactMe.styled.tsx
@@ -1,7 +1,20 @@
 import styled from "styled-components";
 
 export const SocialMedia = styled.div`
-  height: 200px;
-  width: 200px;
-  justify-content: space-between;
+  display: flex;
+  height: 100px;
+  width: 700px;
+  margin: auto;
+  justify-content: space-around;
+`;
+
+export const SocialIcon = styled.img`
+  height: 70px;
+  width: 100px;
+  padding: 50px;
+
+  &: hover {
+    transform: translateY(-7px);
+    filter: drop-shadow(5px 5px 5px rgba(0, 0, 0, 0.3));
+  }
 `;
