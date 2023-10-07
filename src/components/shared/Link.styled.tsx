bdf1f701b6416eb4c9e4421d588cee202f292837
Fozan Rizvi <fozanrizvi@gmail.com>
2023-10-07 11:38:13 -0500

    Link edits

--- src/components/shared/Link.styled.tsx
+++ src/components/shared/Link.styled.tsx
@@ -2,11 +2,14 @@ import styled from "styled-components";
 
 export const Link = styled.a`
   display: block;
+  //background-color: #233142;
+  // background-image: url("../../assets/images/banners/back.jpeg");
+  padding: 20px;
   margin: 10px 0;
   text-decoration: none;
-  color: #000;
+  color: #272343;
   &:hover {
-    color: grey;
+    color: #0881a3;
     text-decoration: underline;
   }
 `;
