9954f616c1031925a1b1cbce2fc019c8403e6ffd
Fozan Rizvi <fozanrizvi@gmail.com>
2024-02-01 14:21:01 -0600

    design fixes for text

--- src/components/shared/Text.styled.tsx
+++ src/components/shared/Text.styled.tsx
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
 export const Text = styled.p`
+  font-size: x-large;
+  display: inline-flex;
   color: #c26b3f;
+  background: -webkit-linear-gradient(
+    0deg,
+    rgba(255, 255, 255, 0.75),
+    rgba(255, 255, 255, 0.25)
+  );
+  backdrop-filter: blur(3px);
 `;
